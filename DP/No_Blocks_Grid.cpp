a7e0dc267f66092e58b3d325114fce6a1b58a05d
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2024-12-24 12:25:04 -0500

    Create No_Blocks_Grid.cpp

---
+++ DP/No_Blocks_Grid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int pascal(int w, int h,  vector<vector<int>> &cache){
+  
+  if (w == 1 | h == 1){ // base case
+    return 1;
+  }
+ 
+  if (cache[w][h] != 0){
+    return cache[w][h]; // check with cache if already solved
+  }
+  else{
+    cache[w][h] = pascal(w-1, h, cache) + pascal(w, h-1, cache); // recursive call
+    return cache[w][h];
+  }
+  
+}
+
+int main (){
+  int w, h;
+  
+  cin >> w >> h;
+  
+ vector<vector<int>> cache(w + 1, vector<int>(h + 1, 0)); // cache initialization, (note it is 2D this time)
+
+  
+  int out = pascal(w,h, cache);
+  
+  cout << out;
+}
