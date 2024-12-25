0a2b63082c8da9a7d3bb1aa81ce8f8482702aaef
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2024-12-25 10:57:20 -0500

    Create Subset_Sum.cpp

---
+++ Subset_Sum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int sum (int t, int n, vector <int> &list, vector<vector<int>> &cache){
+  
+  if (t == 0){
+    return 1;
+  }
+  if(n == 0){
+    return 0;
+  }
+  if (cache[n][t] != -1){
+    return cache[n][t];
+  }
+  else{ // this time the check looks at weather or not a sum is possible
+    if (list[n-1] <= t){
+      cache[n][t] = sum(t, n-1, list, cache) || sum(t - list[n-1], n-1, list, cache);
+      return cache[n][t];
+    }
+    else{
+      cache[n][t] = sum(t, n-1, list, cache);
+      return cache[n][t];
+    }
+  }
+}
+
+int main(){
+  int n, t;
+  vector <int> list;
+  cin >> n;
+  
+  for (int i = 0; i < n; i++){
+    int temp;
+    cin >> temp;
+    list.push_back(temp);
+  }
+  cin >> t;
+  
+  vector<vector<int>> cache(n + 1, vector<int>(t+1, -1)); //cache
+  
+  int out = sum(t,n,list,cache);
+  
+  cout << out;
+}
