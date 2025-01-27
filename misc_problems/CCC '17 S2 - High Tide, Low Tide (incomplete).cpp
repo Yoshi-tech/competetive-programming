06a2b84edb47ac1a93b43ff89519bcfb441fc33b
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2025-01-27 17:57:24 -0500

    Update CCC '17 S2 - High Tide, Low Tide (incomplete).cpp

--- misc_problems/CCC '17 S2 - High Tide, Low Tide (incomplete).cpp
+++ misc_problems/CCC '17 S2 - High Tide, Low Tide (incomplete).cpp
@@ -4,59 +4,42 @@
 
 using namespace std;
 
-vector <int> reconstruct(vector<int> vec, vector<int> &ans){
-  int size = vec.size();
-  int max_val = *max_element(vec.begin(), vec.end());
-  int min_val = *min_element(vec.begin(), vec.end());
-  if(size == 1){
-    ans.push_back(vec[0]);
-    return ans;
-  }
-  if(size == 0){
-    return ans;
-  }
-  
-  else if(size % 2 == 0){
-    ans.push_back(max_val);
-    ans.push_back(min_val);
-
-    vec.erase(remove(vec.begin(), vec.end(), max_val), vec.end());
-    vec.erase(remove(vec.begin(), vec.end(), min_val), vec.end());
-
-   return reconstruct(vec, ans);
-  }
-  else{
-    ans.push_back(min_val);
-    ans.push_back(max_val);
-
-
-    vec.erase(remove(vec.begin(), vec.end(), max_val), vec.end());
-    vec.erase(remove(vec.begin(), vec.end(), min_val), vec.end());
-
-    return reconstruct(vec, ans);
-  }
-  
-  
-}
-
 int main(){
   int n;
   
   cin >> n;
   
   vector <int> vec(n);
-  vector <int> ans;
+  vector <int> solution(n);
   
   for(int i = 0; i < n; i++){
-    int u;
-    cin >> u;
-    vec[i] = u;
+    int v;
+    cin >> v;
+    
+    vec[i] = v;
   }
   
-  reconstruct(vec, ans);
+  sort(vec.begin(), vec.end());
+  vector<int> low(vec.begin(), vec.begin() + n / 2); // First half: Low tides
+  vector<int> high(vec.begin() + n / 2, vec.end()); // Second half: High tides
+  
+  reverse(low.begin(), low.end());
+  int low_indx = 0, high_indx = 0;
+  for(int i = 0; i < n; i++){
+      if(i%2 == 0){
+
+        solution[i] = low[low_indx];
+        low_indx++;
+      }
+      else{
+        solution[i] = high[high_indx];
+        high_indx++;
+      }
+
+    
+  }
   
   for(int i = 0; i < n; i++){
-    cout << ans[i] << " ";
+    cout <<solution[i] << " ";
   }
-  
 }
