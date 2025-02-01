d5e243c633c9104a7da0a41da5d934b27f612a94
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2025-02-01 10:43:16 -0500

    Update and rename CCC '16 S1 - Ragaman(partial).cpp to CCC '16 S1 - Ragaman.cpp

--- misc_problems/CCC '16 S1 - Ragaman.cpp
+++ misc_problems/CCC '16 S1 - Ragaman.cpp
@@ -3,23 +3,20 @@
 #include <string>
 #include <cmath>
 #include <algorithm>
-
 using namespace std;
-
 int main(){
   
   string s, s1;
   
   cin >> s >> s1;
   bool isTrue = true;
-
   
   if(s1.size() != s.size()){
     isTrue = false;
   }
   
-  vector <int> dict(25, 0);
-  vector <int> dict2(26, 0);
+  vector <int> dict(26, 0); // size needed to be 26 not 25, simple silly mistake
+  vector <int> dict2(27, 0);
   
   for(int i = 0; i < s.size(); i++){
     int intr = abs(97 - s[i]);
@@ -28,23 +25,25 @@ int main(){
   }
   for(int i = 0; i < s1.size(); i++){
     if(s1[i] == '*'){
-      dict2[25]++;
+      dict2[26]++;
+      
     }
+    else{
+      int intr = abs(97 - s1[i]);
     
-    int intr = abs(97 - s1[i]);
-    
-    dict2[intr]++;
+      dict2[intr]++;
+    }
   }
+  
   int counter = 0;
-  for(int i = 0; i < 25; i++){
+  for(int i = 0; i < 26; i++){
     if(dict2[i] != dict[i]){
       counter += abs(dict[i] - dict2[i]);
-      //cout << i << ' ' << "at index: " << i+97 << endl;
     }
   }
   
   if(isTrue){
-    if(counter == dict2[25]){
+    if(counter == dict2[26]){
       cout << 'A';
     }
     else{
